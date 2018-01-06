58934c10735acbf698e46d86420c7f4306c428d0
Roger Berlind <roger@hashicorp.com>
2018-01-06 12:28:15 -0500

    fixed zone

--- main.tf
+++ main.tf
@@ -54,5 +54,6 @@ resource "google_compute_instance" "demo" {
 }
 
 output "external_ip"{
-  value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
+  #value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
+  value = "127.0.0.1"
 }
